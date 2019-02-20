1821fc01afa53354ebcd38ee90852c9a6da3be63
Amit <amit@herolo.co.il>
2019-02-20 20:15:55 +0200

    Create search bar content

--- src/containers/searchBar/SearchBar.js
+++ src/containers/searchBar/SearchBar.js
@@ -1,14 +1,47 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { setSearch } from '../../redux/actions/dashboardActions';
 
 class SearchBar extends Component {
     state = {}
+
+    handleChange = (event) => {
+        this.props.setSearch({ search: event.target.value })
+    }
+
     render() {
+        const { dashboardData: { search } } = this.props;
         return (
-            <div>
-                SearchBar
+            <div className='search-bar'>
+                <div className='search-bar__content'>
+                    <div className='search-bar__item'>
+                        <label
+                            htmlFor="text"
+                            className='search-bar__label' >Search:</label>
+                        <input
+                            type="text"
+                            className='search-bar__input'
+                            id='text'
+                            onChange={this.handleChange}
+                            value={search} />
+                    </div>
+                    <div className='search-bar__item'>
+                        <label htmlFor="sort" className='search-bar__label'>Sort By:</label>
+                        <input type="text" className='search-bar__input' id='sort' />
+                    </div>
+                    <div className='search-bar__item search-bar__item-btn'>
+                        <button className='btn btn-primary'>Submit</button>
+                    </div>
+                </div>
             </div>
         );
     }
 }
 
-export default SearchBar;
+const mapStateToProps = ({ dashboardData }) => {
+    return {
+        dashboardData,
+    }
+}
+
+export default connect(mapStateToProps, { setSearch })(SearchBar);
