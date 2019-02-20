022083b4442a33d3f00ea20684871ec77b26c2b6
Amit <amit@herolo.co.il>
2019-02-20 20:43:43 +0200

    Add init state by application state

--- src/redux/reducers/dashboardReducer.js
+++ src/redux/reducers/dashboardReducer.js
@@ -1,15 +1,21 @@
 import * as actionTypes from '../actions/_actionsTypes';
+import storageService from '../../services/storageService';
+const storage = new storageService();
 
 const INIT_STATE = {
     data: [],
-    search: '',
-    sort: { value: 'stars', label: 'Stars' },
+    search: storage.getItem('search') ? storage.getItem('search') : '',
+    sort: storage.getItem('sort') ?
+        { value: storage.getItem('sort'), label: storage.getItem('sort') } :
+        { value: 'stars', label: 'stars' },
 };
+
 
 export default function (state = INIT_STATE, action) {
     switch (action.type) {
         case actionTypes.SET_SEARCH: {
             const { search } = action.payload;
+            storage.store('search', search);
             return {
                 ...state,
                 search,
@@ -17,6 +23,7 @@ export default function (state = INIT_STATE, action) {
         }
         case actionTypes.SET_SORT: {
             const { sort } = action.payload;
+            storage.store('sort', sort.value);
             return {
                 ...state,
                 sort,
