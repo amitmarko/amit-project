c3ddf7cf9aac9eb4c408f0fda52e3eab64e1c88a
Amit <amit@herolo.co.il>
2019-02-20 19:07:24 +0200

    Connect Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import Dashboard from './containers/dashboard/Dashboard'
+import { Route, Switch } from 'react-router'
+import { BrowserRouter } from 'react-router-dom';
 import './sass/main.scss'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        amit
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Switch>
+            <Route path='/' component={Dashboard} />
+          </Switch>
+        </div>
+      </BrowserRouter >
     );
   }
 }
---
+++ src/containers/dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Dashboard extends Component {
+    state = {}
+    render() {
+        return (
+            <div className='dashboard'>
+                dashboard
+            </div>
+        );
+    }
+}
+
+export default Dashboard;
