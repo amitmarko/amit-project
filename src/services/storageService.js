766507dfbc5f1dce1f6d724d219f44006415851d
Amit <amit@herolo.co.il>
2019-02-20 20:43:28 +0200

    Encoded storage

--- src/services/storageService.js
+++ src/services/storageService.js
@@ -1,11 +1,19 @@
 class storageService {
 
     store = (key, val) => {
+        key = window.btoa(key);
+        val = window.btoa(val);
         localStorage.setItem(key, val);
     }
 
     clear = () => {
         localStorage.clear();
+    }
+
+    getItem = (key) => {
+        key = window.btoa(key);
+        const value = localStorage.getItem(key);
+        return value ? window.atob(value) : null;
     }
 
 }
