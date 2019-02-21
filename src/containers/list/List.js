c1b9dfa89e0c06b851479b4517c619c016c56c2b
Amit <amit@herolo.co.il>
2019-02-21 19:31:45 +0200

    Add 'Facebook' loader

--- src/containers/list/List.js
+++ src/containers/list/List.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 import ListItem from '../../components/listItem/ListItem';
+import Loader from '../../components/loader/Loader';
+import { Github } from '../../shared/consts';
 import { connect } from 'react-redux';
 
 class List extends Component {
 
-    state = {}
+    renderLoader = () => {
+        const loader = [];
+        for (let i = 0; i < Github.limit; i++)
+            loader.push(<Loader />);
+        return loader;
+    }
+
+    renderList = () => {
+        const { list } = this.props;
+        return list.map((data, index) => <ListItem data={data} key={index} />)
+    }
 
     render() {
-        const { list, isLoad } = this.props;
+        const { isLoad } = this.props;
         return (
             <div className='list'>
-                {list.map((data, index) => <ListItem data={data} key={index} />)}
+                {isLoad ?
+                    this.renderLoader() :
+                    this.renderList()
+                }
             </div>
         );
     }
