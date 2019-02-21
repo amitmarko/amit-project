5a3a875c54499ab7e8c43c4afad6eb7fbd4a2f7e
Amit <amit@herolo.co.il>
2019-02-21 19:06:37 +0200

    Create listitem component

---
+++ src/components/listItem/ListItem.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+const ListItem = ({ data }) => {
+    const { full_name, description, forks } = data;
+    return (
+        <div className='list__item'>
+            <div className='list__item__row'>
+                <span className='list__item__row-label'>repo</span>
+                <span className='list__item__row-details'>{full_name}</span>
+            </div>
+            <div className='list__item__row'>
+                <span className='list__item__row-label'>description</span>
+                <span className='list__item__row-details'>{description}</span>
+            </div>
+            <div className='list__item__row'>
+                <span className='list__item__row-label'>forks</span>
+                <span className='list__item__row-details'>{forks}</span>
+            </div>
+        </div>
+    );
+}
+
+export default ListItem;
